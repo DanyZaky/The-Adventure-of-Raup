7249f739a21295ae12e8519959480d1fb7281836
Dany Zaky <danyzakydany123@gmail.com>
2021-01-17 23:12:59 +0700

    add project files

---
+++ The Adventure of Raup/Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int jump;
+    private bool jumping = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0,jump);
+        }
+    }
+}
